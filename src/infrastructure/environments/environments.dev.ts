7a8459530fe1eff5d4bb6c54646c15d313b071bc
DevKike <alvaronarvaez.dev@gmail.com>
2025-09-03 17:38:58 -0500

    feat: add HTTP status code enum and custom exception class

--- src/infrastructure/environments/environments.dev.ts
+++ src/infrastructure/environments/environments.dev.ts
@@ -1,3 +1,5 @@
 export const environments = {
   awsRegion: process.env.AWS_REGION || 'us-west-1',
+  usersTableName: process.env.USERS_TABLE_NAME || 'users',
+  saltRounds: process.env.SALT_ROUNDS || 10,
 };
