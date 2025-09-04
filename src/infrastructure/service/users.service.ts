1221855b0f2b98106e10575694c76e7528b52edb
DevKike <alvaronarvaez.dev@gmail.com>
2025-09-04 02:36:31 -0500

    refactor: UsersService to integrate SecretsManagerProvider and HashProvider for user registration

--- src/infrastructure/service/users.service.ts
+++ src/infrastructure/service/users.service.ts
@@ -4,14 +4,34 @@ import {
 } from '../../domain/entity/users.entity.interface';
 import { IUsersRepository } from '../../domain/repository/users.repository.interface';
 import { IUsersService } from '../../domain/service/users.service.interface';
+import { environments } from '../environments/environments.dev';
+
+import { HashProvider } from '../providers/hash.provider';
+import { SecretsManagerProvider } from '../providers/secrets-manager.provider';
 
 export class UsersService implements IUsersService {
-  constructor(private readonly _usersRepository: IUsersRepository) {}
+  private readonly _secretsManagerProvider: SecretsManagerProvider;
+  private readonly _hashProvider: HashProvider;
+
+  constructor(private readonly _usersRepository: IUsersRepository) {
+    this._secretsManagerProvider = new SecretsManagerProvider();
+    this._hashProvider = new HashProvider();
+  }
 
   async register(user: IUserRegisterBody): Promise<IUserRegisterResponse> {
     try {
+      const secret = await this._secretsManagerProvider.get<{ key: string }>(
+        environments.usersSecretsManagerName!
+      );
+
+      const hashedPassword = await this._hashProvider.hash(
+        user.password,
+        secret.key
+      );
+
       const newUser = await this._usersRepository.save({
         ...user,
+        password: hashedPassword,
         address: null,
         phone: null,
         image: null,
