26616d4b23596dc78ff4232084f935fff4513234
DevKike <alvaronarvaez.dev@gmail.com>
2025-09-04 22:51:05 -0500

    feat: add user existence check in update use case with NotFoundException handling

--- src/application/use-cases/update/users-update.use-case.ts
+++ src/application/use-cases/update/users-update.use-case.ts
@@ -4,6 +4,7 @@ import {
   IUserUpdateInput,
   IUserUpdateRes,
 } from '../../../domain/entity/users.entity.interface';
+import { NotFoundException } from '../../../domain/exceptions/not-found.exception';
 import { IUsersService } from '../../../domain/service/users.service.interface';
 import { IUseCase } from '../../../domain/use-case/use-case.interface';
 
@@ -15,6 +16,10 @@ export class UsersUpdateUseCase
   async execute(input: IUserUpdateInput): Promise<IUserUpdateRes> {
     try {
       const { id, ...updateData } = input;
+
+      const user = await this._usersService.getById(id);
+
+      if (!user) throw new NotFoundException('User not found');
 
       return this._usersService.update(id, updateData);
     } catch (error) {
